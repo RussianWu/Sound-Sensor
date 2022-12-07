1e04ef6f1114cd51b99e7f779ddd17171e59a480
RussianWu <russianwu@gmail.com>
2022-12-07 11:43:14 +0100

    Add files via upload
    Upload Traffic Light

---
+++ 5_Sound_Sensor_Traffic_Light/5_Sound_Sensor_Traffic_Light.ino
@@ -0,0 +1,66 @@
+const int sampleWindow = 50;                              // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
+int LEDRed = 11;
+int LEDYellow = 12;
+int LEDGreen = 13;
+
+#define SENSOR_PIN A0
+void setup ()  
+{   
+  pinMode (SENSOR_PIN, INPUT); // Set the signal pin as input 
+  pinMode(LEDRed, OUTPUT);
+  pinMode(LEDYellow, OUTPUT);
+  pinMode(LEDGreen, OUTPUT);
+  Serial.begin(9600);
+}  
+ 
+   
+void loop ()  
+{ 
+   unsigned long startMillis= millis();                   // Start of sample window
+   float peakToPeak = 0;                                  // peak-to-peak level
+ 
+   unsigned int signalMax = 0;                            //minimum value
+   unsigned int signalMin = 4095;                         //maximum value
+ 
+                                                          // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(SENSOR_PIN);                    //get reading from microphone
+      if (sample < 4095)                                  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;                           // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;                           // save just the min levels
+         }
+      }
+   }
+ 
+   peakToPeak = signalMax - signalMin;                    // max - min = peak-peak amplitude
+   int db = map(peakToPeak,50,500,49.5,90);             //calibrate for deciBels
+   Serial.println(db);
+
+   if (db <= 50)
+    { 
+      digitalWrite(LEDGreen, HIGH);
+      digitalWrite(LEDYellow, LOW);
+      digitalWrite(LEDRed, LOW);
+    }
+    else if (db > 50 && db < 75)
+    {
+      digitalWrite(LEDGreen, LOW);
+      digitalWrite(LEDYellow, HIGH);
+      digitalWrite(LEDRed, LOW);
+    }
+    else if (db >= 75)
+    {
+      digitalWrite(LEDGreen, LOW);
+      digitalWrite(LEDYellow, LOW);
+      digitalWrite(LEDRed, HIGH);
+    }
+}
